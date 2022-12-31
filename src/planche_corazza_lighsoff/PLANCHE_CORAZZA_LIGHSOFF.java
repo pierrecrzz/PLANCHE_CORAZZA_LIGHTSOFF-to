8fee2445104a5099766e0acc655555c23056126c
coraz <coraz@LAPTOP-0SM82EUU.home>
2022-12-31 18:18:57 +0100

    Variable cellule de coordonnées x et y Lumiere allumée ou éteinte

---
+++ src/planche_corazza_lighsoff/PLANCHE_CORAZZA_LIGHSOFF.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package planche_corazza_lighsoff;
+
+/**
+ *
+ * @author coraz
+ */
+public class PLANCHE_CORAZZA_LIGHSOFF {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
